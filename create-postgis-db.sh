ce097a2c7498026aaf167343a7c9ff5e1b08cb77
Alexey Ovchinnikov <alexey.ovchinnikov@yandex.ru>
2014-11-30 19:46:17 +0300

    some testing added

--- create-postgis-db.sh
+++ create-postgis-db.sh
@@ -4,8 +4,6 @@ dbname="${WERCKER_POSTGRESQL_DATABASE}${TEST_ENV_NUMBER}"
 user="${WERCKER_POSTGRESQL_USERNAME}"
 password="${WERCKER_POSTGRESQL_PASSWORD}"
 
-echo "Settings: ${dbname} ${user}"
-
 sudo -- su postgres -c "createdb -O ${user} ${dbname}"
 
 if [ -n "$WERCKER_POSTGRESQL_HOST" ]; then
@@ -34,8 +32,8 @@ sudo service postgresql restart
 
 if [ -n "$WERCKER_POSTGRESQL_HOST" ]; then
     echo "creating postgres database ${user}:*****@${WERCKER_POSTGRESQL_HOST}:${WERCKER_POSTGRESQL_PORT}/${dbname} with postgis extensions"
-    PGPASSWORD="${password}" psql -c 'CREATE EXTENSION postgis;' -U "${user}" -d werckerdb$TEST_ENV_NUMBER -h $WERCKER_POSTGRESQL_HOST -p $WERCKER_POSTGRESQL_PORT
-    PGPASSWORD="${password}" psql -c 'CREATE EXTENSION postgis_topology;' -U "${user}" -d werckerdb$TEST_ENV_NUMBER -h $WERCKER_POSTGRESQL_HOST -p $WERCKER_POSTGRESQL_PORT
+    PGPASSWORD="${password}" psql -c 'CREATE EXTENSION postgis;' -U "${user}" -d "${dbname}" -h $WERCKER_POSTGRESQL_HOST -p $WERCKER_POSTGRESQL_PORT
+    PGPASSWORD="${password}" psql -c 'CREATE EXTENSION postgis_topology;' -U "${user}" -d "${dbname}" -h $WERCKER_POSTGRESQL_HOST -p $WERCKER_POSTGRESQL_PORT
 else
     echo "creating postgres database ${user}:*****@${dbname} with postgis extensions"
     sudo -- su "${user}" -c "PGPASSWORD=${password} psql -c 'CREATE EXTENSION postgis;' -d ${dbname}"
