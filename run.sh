b1e131c3894afd0633c3b0206fc56f8c144b2533
Alexey Ovchinnikov <alexey.ovchinnikov@yandex.ru>
2014-11-30 14:30:01 +0300

    command output improvements

--- run.sh
+++ run.sh
@@ -23,6 +23,11 @@ if [ ! -n "$WERCKER_POSTGRESQL_URL" ]; then
 fi
 
 echo "creating postgres database ${WERCKER_POSTGRESQL_USERNAME}:***@${WERCKER_POSTGRESQL_HOST}:${WERCKER_POSTGRESQL_PORT}/${WERCKER_POSTGRESQL_DATABASE} with postgis extensions"
+echo "postgres processes:"
+ps aux | grep postgres
+echo "postgres network connections:"
+netstat -na | grep ${WERCKER_POSTGRESQL_PORT}
+
 sudo -- su postgres -c "PGPASSWORD=${WERCKER_POSTGRESQL_PASSWORD} createdb -h ${WERCKER_POSTGRESQL_HOST} -p ${WERCKER_POSTGRESQL_PORT} -U postgres -O postgres werkerdb$TEST_ENV_NUMBER"
 sudo -- su postgres -c "PGPASSWORD=${WERCKER_POSTGRESQL_PASSWORD} psql -c 'CREATE EXTENSION postgis;' -U postgres -d werckerdb$TEST_ENV_NUMBER -h ${WERCKER_POSTGRESQL_HOST} -p ${WERCKER_POSTGRESQL_PORT}"
 sudo -- su postgres -c "PGPASSWORD=${WERCKER_POSTGRESQL_PASSWORD} psql -c 'CREATE EXTENSION postgis_topology;' -U postgres -d werckerdb$TEST_ENV_NUMBER -h ${WERCKER_POSTGRESQL_HOST} -p ${WERCKER_POSTGRESQL_PORT}"
