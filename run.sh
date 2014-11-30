cf69bb416224e4388186061ff8d2f1481e4dccd0
Alexey Ovchinnikov <alexey.ovchinnikov@yandex.ru>
2014-11-30 14:39:35 +0300

    command output improvements

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ if [ ! -n "$WERCKER_POSTGRESQL_URL" ]; then
 fi
 
 echo "postgres network connections:"
-netstat -na | grep ${WERCKER_POSTGRESQL_PORT}
+netstat -na | grep "${WERCKER_POSTGRESQL_PORT}"
 echo "creating postgres database ${WERCKER_POSTGRESQL_USERNAME}:***@${WERCKER_POSTGRESQL_HOST}:${WERCKER_POSTGRESQL_PORT}/${WERCKER_POSTGRESQL_DATABASE} with postgis extensions"
 
 sudo -- su postgres -c "PGPASSWORD=${WERCKER_POSTGRESQL_PASSWORD} createdb -h ${WERCKER_POSTGRESQL_HOST} -p ${WERCKER_POSTGRESQL_PORT} -U postgres -O postgres werkerdb$TEST_ENV_NUMBER"
