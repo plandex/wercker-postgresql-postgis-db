cc35051eef84c2233bf299313d6712c80521479c
Alexey Ovchinnikov <alexey.ovchinnikov@yandex.ru>
2014-11-30 14:36:27 +0300

    command output improvements

--- run.sh
+++ run.sh
@@ -22,11 +22,9 @@ if [ ! -n "$WERCKER_POSTGRESQL_URL" ]; then
     export WERCKER_POSTGRESQL_URL="postgres://${WERCKER_POSTGRESQL_USERNAME}:${WERCKER_POSTGRESQL_PASSWORD}@${WERCKER_POSTGRESQL_HOST}:${WERCKER_POSTGRESQL_PORT}/${WERCKER_POSTGRESQL_DATABASE}${TEST_ENV_NUMBER}"
 fi
 
-echo "creating postgres database ${WERCKER_POSTGRESQL_USERNAME}:***@${WERCKER_POSTGRESQL_HOST}:${WERCKER_POSTGRESQL_PORT}/${WERCKER_POSTGRESQL_DATABASE} with postgis extensions"
-echo "postgres processes:"
-ps aux | grep postgres
 echo "postgres network connections:"
 netstat -na | grep ${WERCKER_POSTGRESQL_PORT}
+echo "creating postgres database ${WERCKER_POSTGRESQL_USERNAME}:***@${WERCKER_POSTGRESQL_HOST}:${WERCKER_POSTGRESQL_PORT}/${WERCKER_POSTGRESQL_DATABASE} with postgis extensions"
 
 sudo -- su postgres -c "PGPASSWORD=${WERCKER_POSTGRESQL_PASSWORD} createdb -h ${WERCKER_POSTGRESQL_HOST} -p ${WERCKER_POSTGRESQL_PORT} -U postgres -O postgres werkerdb$TEST_ENV_NUMBER"
 sudo -- su postgres -c "PGPASSWORD=${WERCKER_POSTGRESQL_PASSWORD} psql -c 'CREATE EXTENSION postgis;' -U postgres -d werckerdb$TEST_ENV_NUMBER -h ${WERCKER_POSTGRESQL_HOST} -p ${WERCKER_POSTGRESQL_PORT}"
